13ae6f4cbed2fd0c295190de67fbf97e426e1329
marycriv <maryecrivelli@gmail.com>
2019-07-08 10:19:01 -0400

    Added sessions functionality

---
+++ emojigotchi-api/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #authenticate the user
+    user = User.find_by(username: params[:login_creds][:username])
+    if user && user.authenticate(params[:login_creds][:password])
+      log_in(user)
+    else
+      flash.now[:danger] = 'Invalid username/password combination'
+    end
+  end
+
+
+  def destroy
+    log_out
+    redirect_to home_path
+  end
+
+end
--- emojigotchi-api/app/controllers/users_controller.rb
+++ emojigotchi-api/app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
-end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+
+end #End UsersController
